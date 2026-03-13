ee428ec464bf8e64a6d9f5c6e455fd3c3b822e2f
HRNPH <hrnph@protonmail.com>
2026-03-13 15:27:43 +0700

    fix: Image Unop

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: "/cicd-test",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
